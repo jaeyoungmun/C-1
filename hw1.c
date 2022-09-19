1a8bc5bdead9e82d69ce591ab030563581d1936e
jaeyoungmun <mjy0694@naver.com>
2022-09-20 00:50:39 +0900

    cP hw1

--- hw1.c
+++ hw1.c
@@ -1,15 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    int result;
-    int num1, num2, num3;
+	int and1, or1, xor1;
+	int num1, num2;
 
-    printf("세 개의 정수값 입력:");
-    scanf("%d %d %d", &num1, &num2, &num3);
-    
-    result = (num1-num2)*(num2+num3)*(num3%num1)
-    printf("%d * %d * %d = \n", num1-num2, num2+num3, num3%num1);
-    return 0;
+	printf("Input two integers: ");
+	scanf_s("%d %d", &num1, &num2);
+	and1 = num1 & num2;
+	or1 = num1 | num2;
+	xor1 = num1 ^ num2;
+
+	printf(num1, "&", num2, "=", and1);
+	printf(num1, "|", num2, "=", or1);
+	printf(num1, "^", num2, "=", xor1);
+	return 0;
 }
